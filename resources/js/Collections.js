1a8cc29aa441545ff64677988bfafdf729e0c189
RedPhantom <mirpaa.itay@gmail.com>
2017-01-29 08:59:45 +0200

    Implement search capabilities and basic dropdown functionalities. Version update.

--- resources/js/Collections.js
+++ resources/js/Collections.js
@@ -3,12 +3,22 @@
 $(document).ready(
     function () {
         window.collData = localStorage.getItem("collData");
+        if (window.collData == null) {
+            console.log("No data has been uploaded yet. Using sample data.");
+            window.collData = [
+                [1, "Sample Collection",  [4, 847.4, 648.1, 385.6]],
+                [2, "Sample Collection2", [85, 48, 95, 74, 62, 15]]
+            ];
+            console.log(window.collData);
+        }
+        $("#import").css('opacity', '0');
 
+       
     })
 
 function collSearch() {
     var table = document.getElementById("CollList");
-    document.getElementById("table").innerHTML = '<thead><tr><th style="width: 10%;">ICD-9 ID</th><th style="width: 70%;">Long Description</th><th style="width: 20%;">Short Description</th></tr></thead>'; // clean the table
+    document.getElementById("CollList").innerHTML = '<thead><tr><th style="width: 10%;">#</th><th style="width: 20%;">Collectin Name</th><th style="width: 70%;">Collection ID</th></tr></thead>'; // clean the table
 
     var tbody = document.createElement("tbody");
     table.appendChild(tbody);
@@ -21,16 +31,16 @@ function collSearch() {
     var result;
 
     for (var i = 0, len = _data.length; i < len; i++) {
-
-        if (_data[i][1].toLowerCase().includes(_term.toLowerCase())) { // search in second column, long description. INDEX = 1     
+        console.log(_data);
+        if (_data[i][1].toLowerCase().includes(_term.toLowerCase())) { // search in name column, id 1.  
             var row = document.createElement("tr");
             $(row).addClass('selectable') //mark the row as selectable and by clicking trigger the copyToClipboard method (Intro.js)
-            data[i].forEach(function (item) {
+            _data[i].forEach(function (item) {
                 var cell = document.createElement("td");
                 cell.textContent = item;
                 row.appendChild(cell);
 
-                if (document.getElementById("table").rows.length <= _limit) {
+                if (document.getElementById("CollList").rows.length <= _limit) {
                     tbody.appendChild(row);
                 }
             });
@@ -43,10 +53,10 @@ function collSearch() {
 // Upon clicking "remove collection"
 function clickRemove() {
 
-    if (window.isCollRowSelected) {
+    //if (window.isCollRowSelected) {
         $("#removeCollection").popover('show')
         document.getElementById("deleteAlert").removeAttribute("hidden");
-    }
+   // }
 
 }
 
@@ -63,8 +73,13 @@ function declineDelete() {
 
 // Read user-uploaded data file (collection set) and add it to local storage
 function importData() {
-    // Check for the various File API support.
-
+    // creating input on-the-fly
+    var input = $(document.createElement('input'));
+    input.attr("type", "file");
+    input.trigger('click'); // opening dialog
+    return false; // avoiding navigation
+    
+    
 
 }
 
